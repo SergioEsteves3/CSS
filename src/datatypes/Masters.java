f166db820d740429828e6b623468ac7c0db133ad
Sergio Esteves <sergioesteves2000@hotmail.com>
2024-04-02 13:44:44 +0100

    Added javadoc for Masters enum

--- src/datatypes/Masters.java
+++ src/datatypes/Masters.java
@@ -1,5 +1,11 @@
 package datatypes;
-
+/**
+ * Enum for Masters degrees
+ * @author Henrique Vale 58168
+ * @author Leonardo Monteiro 58250
+ * @author Sergio Esteves 58245
+ *
+ */
 public enum Masters {
 	COMPUTER_ENGINEERING,
 	DATA_SCIENCE,
